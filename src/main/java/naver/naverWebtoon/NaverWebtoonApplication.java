9e11ac05bf95855aef28aaca25e214c5293d3a94
JoonwooKim <kkn8010@gmail.com>
2025-03-03 16:21:21 +0900

    first commit

---
+++ src/main/java/naver/naverWebtoon/NaverWebtoonApplication.java
@@ -0,0 +1,13 @@
+package naver.naverWebtoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaverWebtoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaverWebtoonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/naver/naverWebtoon/NaverWebtoonApplicationTests.java
@@ -0,0 +1,13 @@
+package naver.naverWebtoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaverWebtoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
